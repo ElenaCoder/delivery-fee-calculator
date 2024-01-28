1db93082e04cc74429574b10a099e118c2d4c414
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-01-28 22:58:53 +0200

    feat: add unit tests for calculateItemsSurcharge function

---
+++ src/utils/calculateItemsSurcharge.test.ts
@@ -0,0 +1,20 @@
+import { calculateItemsSurcharge } from './calculateItemsSurcharge.ts';
+
+describe('calculateItemsSurcharge', () => {
+    const testCases = [
+      { amountOfItems: 4, expected: 0 },
+      { amountOfItems: 5, expected: 0.5 },
+      { amountOfItems: 10, expected: 3 },
+      { amountOfItems: 13, expected: 5.7 },
+      { amountOfItems: 14, expected: 6.2 },
+      { amountOfItems: 0, expected: 0 },
+      { amountOfItems: 1, expected: 0 },
+    ];
+
+    testCases.forEach(({ amountOfItems, expected }) => {
+      test(`Items: ${amountOfItems} - Surcharge: ${expected}`, () => {
+        const result = calculateItemsSurcharge(amountOfItems);
+        expect(result).toBe(expected);
+      });
+    });
+  });
