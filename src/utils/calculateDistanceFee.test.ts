3ccfcc43443ac28c211daa05b512e831c2518fb2
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-01-29 00:05:29 +0200

    refactor: apply forced styling to calculateDistanceFee.test.ts

--- src/utils/calculateDistanceFee.test.ts
+++ src/utils/calculateDistanceFee.test.ts
@@ -1,20 +1,20 @@
 import { calculateDistanceFee } from './calculateDistanceFee.ts';
 
 describe('calculateDistanceFee', () => {
-  const testCases = [
-    { deliveryDistance: 800, expected: 2 },
-    { deliveryDistance: 1000, expected: 2 },
-    { deliveryDistance: 1499, expected: 3 },
-    { deliveryDistance: 1500, expected: 3 },
-    { deliveryDistance: 1501, expected: 4 },
-    { deliveryDistance: 2000, expected: 4 },
-    { deliveryDistance: 2001, expected: 5 },
-  ];
+    const testCases = [
+        { deliveryDistance: 800, expected: 2 },
+        { deliveryDistance: 1000, expected: 2 },
+        { deliveryDistance: 1499, expected: 3 },
+        { deliveryDistance: 1500, expected: 3 },
+        { deliveryDistance: 1501, expected: 4 },
+        { deliveryDistance: 2000, expected: 4 },
+        { deliveryDistance: 2001, expected: 5 },
+    ];
 
-  testCases.forEach(({ deliveryDistance, expected }) => {
-    test(`Calculate distance fee for ${deliveryDistance} meters`, () => {
-      const fee = calculateDistanceFee(deliveryDistance);
-      expect(fee).toBe(expected);
+    testCases.forEach(({ deliveryDistance, expected }) => {
+        test(`Calculate distance fee for ${deliveryDistance} meters`, () => {
+            const fee = calculateDistanceFee(deliveryDistance);
+            expect(fee).toBe(expected);
+        });
     });
-  });
 });
