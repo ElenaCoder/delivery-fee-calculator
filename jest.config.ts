fad753c6df8250a85b60b6042c6438726365f415
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-01-28 22:56:52 +0200

    feat: import jest, ts-jest, ts-node modules for testing purpose

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    transform: {
+      '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  };
