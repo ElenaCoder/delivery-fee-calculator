669417097ccec8f7700e395b530fc32f51d932d5
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-01-28 22:53:34 +0200

    feat: implement isDuringRushHours function

---
+++ src/utils/isDuringRushHours.ts
@@ -0,0 +1,9 @@
+export function isDuringRushHours(date: Date): boolean {
+    const currentHour: number = date.getHours();
+    const isFriday: boolean = date.getDay() === 5;
+
+    const rushHoursStart: number = 15;
+    const rushHoursEnd: number = 19;
+
+    return isFriday && (rushHoursStart <= currentHour && currentHour < rushHoursEnd);
+}
